c8f7a35deca3873cfd02fd996668078c8a096d00
thiago21sousa21 <thiago21sousa21@gmail.com>
2024-12-23 12:27:26 -0300

    feat: criando pagina de login

--- aulas/src/pages/login/Login.tsx
+++ aulas/src/pages/login/Login.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import {useNavigate} from "react-router-dom"
 
 export const Login = () => {
     const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
 
     const handleClick = ()=>{
         navigate('/pagina-inicial')
     }
-
     return(
-        <div onClick={handleClick}>
-            Login
+        <div>
+            <form>
+                <label>
+                    <span> Email </span>
+                    <input type="text" value={email} onChange={e =>setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span> Senha </span>
+                    <input type="password" value={senha} onChange={e =>setSenha(e.target.value)}/>
+                </label>
+            </form>
+            <button onClick={handleClick}>Pagina Inicial</button>
         </div>
+      
     )
 }
