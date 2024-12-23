c7337604d9e3ce75a79c59821487e6f9d054e0a4
thiago21sousa21 <thiago21sousa21@gmail.com>
2024-12-23 10:40:59 -0300

    feat: adicionando dashboard e criando a rota * para pagina incial

--- aulas/src/routes/index.tsx
+++ aulas/src/routes/index.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes as Switch, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "../pages";
 
 export const Routes = ()=>{
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" element={<h1>Olá</h1>}/>
+                <Route path="/pagina-inicial" element={<Dashboard/>}/>
+                <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
             </Switch>
         </BrowserRouter>
     )
